c14c19a17785444af1c91d4b31fcbac8726fd304
Aditya Sharma <aditya.sharma@hotwaxsystems.com>
2021-11-17 13:26:24 +0530

    Revert: console logs (#3bdgrp)

--- src/views/update-password.vue
+++ src/views/update-password.vue
@@ -125,7 +125,7 @@ export default defineComponent({
   },
   methods: {
     async updatePassword(form, resetForm, validate) {
-      const result = await validate()\
+      const result = await validate()
       if (!result.valid) {
            return;
       }
