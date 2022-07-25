4944430acb82f76e206222c96a6968778985c163
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-07-25 17:46:50 +0530

    Improved: label to eCom Store on Settings.vue(#23tw4yf)

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -11,7 +11,7 @@
 
     <ion-content>
       <ion-item>
-        <ion-label>{{ $t("Brand") }}</ion-label>
+        <ion-label>{{ $t("eCom Store") }}</ion-label>
         <ion-select @ionChange="updateBrand($event)" interface="popover" :value="selectedBrand">
           <ion-select-option value="">{{ $t("All") }}</ion-select-option>
           <ion-select-option v-bind:key="brand.id" v-for="brand in brands" :value="brand.id">{{ brand.name }}</ion-select-option>
