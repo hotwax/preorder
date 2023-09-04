d6223cf2fb661f780a16a49db7a7976595a775e7
Sanskar Soni <sonisanskar223@gmail.com>
2023-09-04 10:43:52 +0530

    Improved: variables names and alert function (#175)

--- src/views/catalog-product-details.vue
+++ src/views/catalog-product-details.vue
@@ -765,12 +765,12 @@ export default defineComponent({
         console.error(error)
       }
     },
-    async showAlert(header: string, message: string, cancelButtonLabel: string, successButtonLabel: string){
+    async showAlert(header: string, message: string, successButtonLabel: string){
       const alert = await alertController.create({
         header: translate(header),
         message: translate(message),
         buttons: [{
-          text: translate(cancelButtonLabel),
+          text: translate('Cancel'),
           role: 'cancel'
         },
         {
@@ -789,10 +789,10 @@ export default defineComponent({
 
       const isChecked = event.target.checked;
       const successButtonLabel = isChecked ? 'disable' : 'enable';
-      const alertHeader = isChecked ? 'Disable Reserve Inventory?' : 'Enable Reserve Inventory?';
-      const alertMessage = isChecked ? 'Disabling inventory reservations prevents committed inventory from being reduced until it has been shipped. Orders that are pending allocation or haven’t been shipped will not be reduced from sellable inventory.' : 'Enabling inventory reservations reduces inventory counts for committed inventory before it has been shipped. Committed inventory includes orders waiting to be brokered or waiting to be shipped.';
+      const header = isChecked ? 'Disable Reserve Inventory?' : 'Enable Reserve Inventory?';
+      const message = isChecked ? 'Disabling inventory reservations prevents committed inventory from being reduced until it has been shipped. Orders that are pending allocation or haven’t been shipped will not be reduced from sellable inventory.' : 'Enabling inventory reservations reduces inventory counts for committed inventory before it has been shipped. Committed inventory includes orders waiting to be brokered or waiting to be shipped.';
 
-      if (await this.showAlert(alertHeader, alertMessage, 'Cancel', successButtonLabel)) {
+      if (await this.showAlert(header, message, successButtonLabel)) {
         event.target.checked = !isChecked;
       } else {
         return;
@@ -823,10 +823,10 @@ export default defineComponent({
 
       const isChecked = event.target.checked;
       const successButtonLabel = isChecked ? 'disable' : 'enable';
-      const alertHeader = isChecked ? 'Disable Hold Pre-order Physical Inventory?' : 'Enable Hold Pre-order Physical Inventory?';
-      const alertMessage = isChecked ? 'Disabling this setting will push excess physical inventory for pre-sell products online and start selling them as in-stock items.' : 'Enabling this setting will prevent pre-selling products from publishing physical inventory online until their pre-selling queue is cleared.';
+      const header = isChecked ? 'Disable Hold Pre-order Physical Inventory?' : 'Enable Hold Pre-order Physical Inventory?';
+      const message = isChecked ? 'Disabling this setting will push excess physical inventory for pre-sell products online and start selling them as in-stock items.' : 'Enabling this setting will prevent pre-selling products from publishing physical inventory online until their pre-selling queue is cleared.';
 
-      if (await this.showAlert(alertHeader, alertMessage, 'Cancel', successButtonLabel)) {
+      if (await this.showAlert(header, message, successButtonLabel)) {
         event.target.checked = !isChecked;
       } else {
         return;
