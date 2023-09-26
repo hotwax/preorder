b6305d1017ef860adad756197a4d7940fa8a06c0
amansinghbais <singh11amanbais@gmail.com>
2023-09-26 15:03:09 +0530

    Improved: indentation of vue.config file (#178)

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,21 @@
 const path = require('path')
 require("@hotwax/app-version-info")
 module.exports = {
-    pluginOptions: {
-      i18n: {
-        locale: 'en',
-        fallbackLocale: 'en',
-        localeDir: 'locales',
-        enableInSFC: true
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        vue: path.resolve('./node_modules/vue')
       }
-    },
-    configureWebpack: {
-      resolve: {
-        alias: {
-          vue: path.resolve('./node_modules/vue')
-        }
-      }
-    },
-    runtimeCompiler: true,
-    transpileDependencies: ['@hotwax/dxp-components']
-  }
+    }
+  },
+  runtimeCompiler: true,
+  transpileDependencies: ['@hotwax/dxp-components']
+}
