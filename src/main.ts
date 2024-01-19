1de704fe1cd10e526f061c4a79173543fe07fb31
shanmukhdutt <ksssaidutt@gmail.com>
2024-01-19 14:19:12 +0530

    Fixed: Service worker registration error(dxp-253)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
-import './registerServiceWorker'
 
 import { IonicVue } from '@ionic/vue';
 import i18n from './i18n'
