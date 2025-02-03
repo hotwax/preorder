37a0307cdc9d52842ecb274473501ab4339c7fcd
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2025-02-03 19:37:17 +0530

    Fixed: Build failure issue.(#312)

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,6 @@ import '@ionic/vue/css/text-alignment.css';
 import '@ionic/vue/css/text-transformation.css';
 import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/display.css';
-import '@ionic/vue/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
