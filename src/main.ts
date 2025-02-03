c4887bd31c6a4dc6cd88d22526353f51f040b88b
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2025-02-03 18:09:26 +0530

    Enhancement: Added support for the dark mode.

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ import '@ionic/vue/css/text-alignment.css';
 import '@ionic/vue/css/text-transformation.css';
 import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/display.css';
+import '@ionic/vue/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
