9f3b5979d655861c780faff04e47c3016c72740d
R-Sourabh <rsourabh002@gmail.com>
2025-10-17 07:35:20 +0530

    Improved: "Go to Launchpad" button will be hidden in standalone mode and will be permission-driven (dxp/376)

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -25,7 +25,7 @@
             </ion-card-header>
           </ion-item>
           <ion-button color="danger" @click="logout()">{{ $t("Logout") }}</ion-button>
-          <ion-button fill="outline" @click="goToLaunchpad()">
+          <ion-button :standalone-hidden="!hasPermission(Actions.APP_PWA_STANDALONE_ACCESS)" fill="outline" @click="goToLaunchpad()">
             {{ $t("Go to Launchpad") }}
             <ion-icon slot="end" :icon="openOutline" />
           </ion-button>
@@ -206,5 +206,11 @@ export default defineComponent({
     justify-content: space-between;
     align-items: center;
     padding: var(--spacer-xs) 10px 0px;
+  }
+  /* Added conditional hiding in standalone mode that respects user permissions */
+  @media (display-mode: standalone) {
+    [standalone-hidden] {
+      display: none;
+    }
   }
 </style>
